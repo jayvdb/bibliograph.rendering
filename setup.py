713b80f3c7870aa82ab65d4c82e206dcca059833
zopyx <info@zopyx.com>
2010-03-19 03:41:11 +0000

    updated

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.0.0c3' 
+version = '1.0.0' 
 
 long_description = (
     read('README.txt')
